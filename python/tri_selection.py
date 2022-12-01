bd1f6232a91cb8763a294c806f209887b4b7740e
Aouadi <aouadilina04@gmail.com>
2022-12-01 10:22:55 +0100

    function_triselection

--- python/tri_selection.py
+++ python/tri_selection.py
@@ -1,10 +1,12 @@
 from random import randrange
 A = [randrange(0, 20) for i in range(20)]
 print(A)
-for i in range(len(A)):
-    for j in range(i + 1, len(A)):
-        if A[i] > A[j]:
-            A[i], A[j] = A[j], A[i]
-print(A)
+def tri_selection(A):
+    for i in range(len(A)):
+        for j in range(i + 1, len(A)):
+            if A[i] > A[j]:
+                A[i], A[j] = A[j], A[i]
+    return A
+print(tri_selection(A))
 
 
