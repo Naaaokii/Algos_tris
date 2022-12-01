d6a6bccc17503f3c9dcc621ed4b2d4dbb616498b
nom1 <eloitranchant@mbp-de-admin-2.edu.sir>
2022-12-01 10:42:21 +0100

    tri_bulles dans test

--- python/test_tris.py
+++ python/test_tris.py
@@ -1,30 +1,39 @@
 # Tri bulles
 
+def tri_bulles(t):
+    l = len(t)
+    for i in range (l-1, 1, -1):
+        for j in range(0,i):
+            if t[j+1] < t[j]:
+                tab = t[j+1]
+                t[j+1] = t[j]
+                t[j] = tab
+    return t 
 
 # Tri bulles mieux
 
-def tri_bulles_mieux(A):
+def tri_bulles_mieux(t):
     Continue = True
     passage = 0
     while Continue == True:
         Continue = False  # it is assumed that there are no more exchanges to be made
         passage += 1
-        for beginning in range(0, len(A) - passage):
-            if A[beginning] > A[beginning + 1]:
+        for beginning in range(0, len(t) - passage):
+            if t[beginning] > t[beginning + 1]:
                 Continue = True  # we must continue because an exchange has been made
-                provisional = A[beginning + 1]
-                A[beginning + 1] = A[beginning]
-                A[beginning] = provisional
-    return A
+                provisional = t[beginning + 1]
+                t[beginning + 1] = t[beginning]
+                t[beginning] = provisional
+    return t
 
 # Tri Selection
 
-def tri_selection(A):
-    for i in range(len(A)):
-        for j in range(i + 1, len(A)):
-            if A[i] > A[j]:
-                A[i], A[j] = A[j], A[i]
-    return A
+def tri_selection(t):
+    for i in range(len(t)):
+        for j in range(i + 1, len(t)):
+            if t[i] > t[j]:
+                t[i], t[j] = t[j], t[i]
+    return t
 
 # Tri Insertion
 
