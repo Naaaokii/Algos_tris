aa75627721e44964744c7383db6f4a749b96842b
Samba Champroux <sambachamproux@air-de-samba.edu.sir>
2022-12-01 11:56:49 +0100

    tri_insertion 10k

--- python/tri_insertion.py
+++ python/tri_insertion.py
@@ -1,5 +1,20 @@
 from random import randrange
-my_tab= [randrange(100) for i in range(20)]
+my_tab = [randrange(101) for i in range(20)]
 print(my_tab)
-print(min(my_tab))
-# importer 
+
+#min_values= (min(my_tab))
+#print(min_values)
+
+for i in range (1, len(my_tab)):
+    a = my_tab[i]
+    b = i-1
+    while b >= 0 and my_tab[b] > a: 
+        my_tab[b + 1] = my_tab[b] 
+        b -= 1
+    my_tab[b + 1] = a      
+
+print(my_tab) 
+# tab_sorted= []
+# import 
+# my_tab[0]= min_values
+#while
