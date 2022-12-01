ce6fcbd67842d7a0237e2687e64fe070af495101
nom1 <eloitranchant@mbp-de-admin-2.edu.sir>
2022-12-01 15:47:17 +0100

    fonction stat

--- python/tri_bulles.py
+++ python/tri_bulles.py
@@ -2,24 +2,27 @@ from random import randrange
 t = [1,3,9,95,6,2,1]
 print(t)
 def tri_bulles(t):
-    echange = 0
+    compteur = 0
     l = len(t)
     for i in range (l-1, 1, -1):
         for j in range(0,i):
+            compteur+=1
             if t[j+1] < t[j]:
                 tab = t[j+1]
                 t[j+1] = t[j]
                 t[j] = tab
-                echange+=1
-    print(echange)
-    return t 
+                compteur+=3
+    return compteur 
 print(tri_bulles(t))
 
 def stat(min, max, step, nbr):
-    for i in range(min, max, step):
+    for i in range(min, max+1, step):
+        moyenne = 0
         for j in range(nbr):
             t= [randrange(100) for i in range(i)]
-            print(t)
+            moyenne += tri_bulles(t)
+        moyenne = moyenne/nbr    
+        print(i, moyenne)
     
 
 print(stat(10,20,5,10))
