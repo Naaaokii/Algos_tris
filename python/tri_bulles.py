5e9413ee56aca8b3b4ce94053ec4201604369fbb
nom1 <eloitranchant@mbp-de-admin-2.edu.sir>
2022-12-01 14:48:03 +0100

    test method

--- python/tri_bulles.py
+++ python/tri_bulles.py
@@ -1,7 +1,8 @@
 from random import randrange
-t = [randrange(0,10) for i in range(10)]
+t = [1,3,9,95,6,2,1]
 print(t)
 def tri_bulles(t):
+    echange = 0
     l = len(t)
     for i in range (l-1, 1, -1):
         for j in range(0,i):
@@ -9,5 +10,13 @@ def tri_bulles(t):
                 tab = t[j+1]
                 t[j+1] = t[j]
                 t[j] = tab
+                echange+=1
+    print(echange)
     return t 
 print(tri_bulles(t))
+
+def stat(min, max, step, nbr):
+    t = [randrange(min, max, step) for i in range(nbr)]
+    return t
+
+print(stat(10,20,5,10))
