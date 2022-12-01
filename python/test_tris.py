95aa83b4738d101e2a342ef0a42be16b3bce0136
nom1 <eloitranchant@mbp-de-admin-2.edu.sir>
2022-12-01 10:28:58 +0100

    ajout du tri selection dans les test

--- python/test_tris.py
+++ python/test_tris.py
@@ -3,22 +3,28 @@
 
 # Tri bulles mieux
 
-def tri_bulles_mieux(T):
+def tri_bulles_mieux(A):
     Continue = True
     passage = 0
     while Continue == True:
         Continue = False  # it is assumed that there are no more exchanges to be made
         passage += 1
-        for beginning in range(0, len(T) - passage):
-            if T[beginning] > T[beginning + 1]:
+        for beginning in range(0, len(A) - passage):
+            if A[beginning] > A[beginning + 1]:
                 Continue = True  # we must continue because an exchange has been made
-                provisional = T[beginning + 1]
-                T[beginning + 1] = T[beginning]
-                T[beginning] = provisional
-    return T
+                provisional = A[beginning + 1]
+                A[beginning + 1] = A[beginning]
+                A[beginning] = provisional
+    return A
 
 # Tri Selection
 
+def tri_selection(A):
+    for i in range(len(A)):
+        for j in range(i + 1, len(A)):
+            if A[i] > A[j]:
+                A[i], A[j] = A[j], A[i]
+    return A
 
 # Tri Insertion
 
