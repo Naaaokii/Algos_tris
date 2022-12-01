14082fe063c8a1c0421ade91e540800f031192c0
nom1 <eloitranchant@mbp-de-admin-2.edu.sir>
2022-12-01 10:17:57 +0100

    test pour les tris

--- python/test_tris.py
+++ python/test_tris.py
@@ -1,3 +1,26 @@
+# Tri bulles
+
+
+# Tri bulles mieux
+
+def tri_bulles_mieux(T):
+    Continue = True
+    passage = 0
+    while Continue == True:
+        Continue = False  # it is assumed that there are no more exchanges to be made
+        passage += 1
+        for beginning in range(0, len(T) - passage):
+            if T[beginning] > T[beginning + 1]:
+                Continue = True  # we must continue because an exchange has been made
+                provisional = T[beginning + 1]
+                T[beginning + 1] = T[beginning]
+                T[beginning] = provisional
+    return T
+
+# Tri Selection
+
+
+# Tri Insertion
 
 # Avec un tableau déjà trié
 
