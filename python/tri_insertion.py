ec9705d2d41fd33deef643f1bb87cb8c7f81d28b
Samba Champroux <sambachamproux@air-de-samba.edu.sir>
2022-12-01 12:01:51 +0100

    création fonction

--- python/tri_insertion.py
+++ python/tri_insertion.py
@@ -4,16 +4,17 @@ print(my_tab)
 
 #min_values= (min(my_tab))
 #print(min_values)
+def tri_insertion(my_tab):
+    for i in range (1, len(my_tab)):
+        a = my_tab[i]
+        b = i-1
+        while b >= 0 and my_tab[b] > a: 
+            my_tab[b + 1] = my_tab[b] 
+            b -= 1
+        my_tab[b + 1] = a 
+    return my_tab
 
-for i in range (1, len(my_tab)):
-    a = my_tab[i]
-    b = i-1
-    while b >= 0 and my_tab[b] > a: 
-        my_tab[b + 1] = my_tab[b] 
-        b -= 1
-    my_tab[b + 1] = a      
-
-print(my_tab) 
+print(tri_insertion(my_tab)) 
 # tab_sorted= []
 # import 
 # my_tab[0]= min_values
