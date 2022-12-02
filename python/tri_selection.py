cf0172a2e49c5ccd9b0b26b234836aa1482a11dc
matteo <matlam2712@gmail.com>
2022-12-02 10:49:19 +0100

    stat selection

--- python/tri_selection.py
+++ python/tri_selection.py
@@ -2,11 +2,25 @@ from random import randrange
 A = [randrange(0, 20) for i in range(20)]
 print(A)
 def tri_selection(A):
+    compteur = 0
     for i in range(len(A)):
         for j in range(i + 1, len(A)):
+            compteur+=1
             if A[i] > A[j]:
                 A[i], A[j] = A[j], A[i]
+                compteur+=3
     return A
 print(tri_selection(A))
 
 
+def stat(min, max, step, nbr):
+    for i in range(min, max+1, step):
+        moyenne = 0
+        for j in range(nbr):
+            t= [randrange(100) for i in range(i)]
+            moyenne += tri_selection(t)
+        moyenne = moyenne/nbr    
+        print(i, moyenne)
+    return "Calculs finis"
+    
+print(stat(10,20,5,10))
