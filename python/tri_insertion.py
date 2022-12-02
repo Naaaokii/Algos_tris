ac43dffe1a7090448af2df80667638099b4d9720
nom2 <nom2@email.com>
2022-12-02 14:13:29 +0100

    compteur insertion

--- python/tri_insertion.py
+++ python/tri_insertion.py
@@ -5,14 +5,17 @@ print(my_tab)
 #min_values= (min(my_tab))
 #print(min_values)
 def tri_insertion(my_tab):
+    compteur = 0
     for i in range (1, len(my_tab)):
         a = my_tab[i]
         b = i-1
+        compteur+=1
         while b >= 0 and my_tab[b] > a: 
             my_tab[b + 1] = my_tab[b] 
             b -= 1
+            compteur+=3
         my_tab[b + 1] = a 
-    return my_tab
+    return compteur
 
 print(tri_insertion(my_tab)) 
 # tab_sorted= []
